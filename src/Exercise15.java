211e98a56390cf3b30b6c8f24d7c954b30af3b6b
Mirgisa <mirgisakitata@gmail.com>
2025-01-21 12:31:02 +0300

    Java program to extract a portion of an array list.

--- src/Exercise15.java
+++ src/Exercise15.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Exercise15 {
+    public static void main(String[] args) {
+        //15.	Write a Java program to extract a portion of an array list
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        int startIndex =2; //inclusive
+        int endIndex = 4; //exclusive
+
+        List<Integer> subList = list.subList(startIndex, endIndex);
+        System.out.println(subList);
+
+    }
+
 }
