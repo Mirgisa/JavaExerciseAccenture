f29a4ec4e30402c93bee15f82a3fea8a4cef3a87
Mirgisa <mirgisakitata@gmail.com>
2025-01-21 12:30:09 +0300

    Java program to sort a given array list.

--- src/Exercise14.java
+++ src/Exercise14.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Exercise14 {
+    public static void main(String[] args){
+        //14.Write a Java program to sort a given array list
+        int[] lists ={5,1,3,6,7,8,9,2};
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < lists.length; i++) {
+            arrayList.add(lists[i]);
+        }
+        arrayList.sort(Integer::compareTo);
+        System.out.println("Sorted array list: " + arrayList);
+
+
+    }
 }
