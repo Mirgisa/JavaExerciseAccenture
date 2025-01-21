2e8fea64e5458286d052efd785699681d0bb473c
Mirgisa <mirgisakitata@gmail.com>
2025-01-21 12:31:45 +0300

    Java program to insert an element into the array list at the first position.

--- src/Exercise16.java
+++ src/Exercise16.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Exercise16 {
+    public static void main(String[] args) {
+        //16.Write a Java program to insert an element into the array list at the first position
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(2);
+        arrayList.add(4);
+        arrayList.add(6);
+        arrayList.add(8);
+
+        System.out.println("Original ArrayList: " + arrayList);
+
+        arrayList.add(0, 1);
+
+        System.out.println("ArrayList after inserting at the first position: " + arrayList);
+    }
 }
