35b21615a1d1a9804bc0660bbf408b7fc0c2cbf1
Mirgisa <mirgisakitata@gmail.com>
2025-01-21 12:29:28 +0300

    Java program to search for an element in an array list.

--- src/Exercise13.java
+++ src/Exercise13.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Exercise13 {
+    public static void main(String[] args){
+        //13.Write a Java program to search for an element in an array list that enter by user
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(4);
+        arrayList.add(5);
+
+        int element = 3;
+        if( arrayList.contains(element)) {
+            System.out.println("Element found: " + element);
+            System.out.println("Index of element: " + arrayList.indexOf(element));
+        } else {
+            System.out.println("Element not found: ");
+        }
+
+    }
+
 }
