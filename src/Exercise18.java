c43aea7ffa63f168abd017269d15744696fd522f
Mirgisa <mirgisakitata@gmail.com>
2025-01-21 12:33:02 +0300

    Java program to remove a specified element from a linked list.

--- src/Exercise18.java
+++ src/Exercise18.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.LinkedList;
 
 public class Exercise18 {
+    public static void main(String[] args) {
+        //18.	Write a Java program to remove a specified element from a linked list
+        LinkedList<String> list = new LinkedList<String>();
+        list.add("Hello");
+        list.add("World");
+        list.add("It");
+        list.add("Is");
+        list.add("Java");
+        System.out.println("Linked List before removing element : " + list);
+       list.remove("World");
+        // Alternative method to remove an element by index for integer
+       // list.remove(list.indexOf(0));
+        System.out.println("Linked List after removed element : " + list);
+    }
 }
