8947f2872cd9a661fefe44b98c3e1f6b680d8e10
Mirgisa <mirgisakitata@gmail.com>
2025-01-22 15:44:30 +0300

    second commit

--- src/Exercise24.java
+++ src/Exercise24.java
@@ -1,5 +1,16 @@
+import java.util.HashSet;
+
 public class Exercise24 {
     public static void main(String[] args) {
-        //
+        //24.	Write a Java program to empty an hash set
+        HashSet<Integer> hashSet = new HashSet<>();
+        hashSet.add(5);
+        hashSet.add(3);
+        hashSet.add(1);
+        hashSet.add(4);
+        hashSet.add(2);
+        System.out.println("Original HashSet: " + hashSet);
+        hashSet.clear();
+        System.out.println("Empty HashSet: " + hashSet);
     }
 }
