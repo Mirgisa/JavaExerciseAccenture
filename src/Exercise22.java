ccc6f78ec804b7d7b9b1f03748ddfae033ca8184
Mirgisa <mirgisakitata@gmail.com>
2025-01-22 15:39:39 +0300

    Java program to convert a hash set to an array.

--- src/Exercise22.java
+++ src/Exercise22.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.HashSet;
 
 public class Exercise22 {
+    public static void main(String[] args) {
+        //22.	Write a Java program to convert a hash set to an array
+        HashSet<String> hashSet = new HashSet<String>();
+        hashSet.add("Apple");
+        hashSet.add("Banana");
+        hashSet.add("Cherry");
+        System.out.println("Elements in the hash set: "+ hashSet);
+        String[] array = hashSet.toArray(new String[0]);
+
+        System.out.println("Elements in the array)");
+        for (String element : array) {
+            System.out.println(element);
+        }
+
+    }
 }
