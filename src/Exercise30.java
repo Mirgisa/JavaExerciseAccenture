1b87fdd4f4d3ab3ef88bdbe1d3f1a22a4e29e5e1
Mirgisa <mirgisakitata@gmail.com>
2025-01-25 13:34:08 +0300

    Java program to search for a key in a Tree Map.

--- src/Exercise30.java
+++ src/Exercise30.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.TreeMap;
 
 public class Exercise30 {
+    public static void main(String[] args) {
+        //30.	Write a Java program to search for a key in a Tree Map
+        TreeMap<String,Integer> treeMap = new TreeMap<String,Integer>();
+        treeMap.put("A", 1);
+        treeMap.put("B", 2);
+        treeMap.put("C", 3);
+        treeMap.put("D", 4);
+
+        String key = "B";
+        if (treeMap.containsKey(key)) {
+            System.out.println("Key found: " + treeMap.get(key));
+        } else {
+            System.out.println("Key not found: " + key);
+        }
+    }
 }
