4ec224bcdc8618342b104c0b1023bb26e0b202ca
Mirgisa <mirgisakitata@gmail.com>
2025-01-25 13:32:25 +0300

    Java program to get a shallow copy of a HashMap instance.

--- src/Exercise28.java
+++ src/Exercise28.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
 
 public class Exercise28 {
+    public static void main(String[] args) {
+        //28.	Write a Java program to get a shallow copy of a HashMap instance
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("A", 1);
+        map.put("B", 2);
+        map.put("C", 3);
+
+        HashMap<String, Integer> shallowCopy = new HashMap<>(map);
+        System.out.println("Original Map: " + map);
+        System.out.println("Shallow Copy: " + shallowCopy);
+
+        // Modifying the original map will not affect the shallow copy
+        map.put("D", 4);
+        System.out.println("Modified Original Map: " + map);
+        System.out.println("Shallow Copy: " + shallowCopy);
+    }
 }
