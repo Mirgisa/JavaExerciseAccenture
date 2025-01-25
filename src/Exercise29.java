dfefe02138efadb14b21c077fb8f5550d26687ba
Mirgisa <mirgisakitata@gmail.com>
2025-01-25 13:33:19 +0300

    Java program to get the value of a specified key in a map.

--- src/Exercise29.java
+++ src/Exercise29.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Exercise29 {
+    public static void main(String[] args) {
+        //29.	Write a Java program to get the value of a specified key in a map
+        Map<String, Integer> map = new HashMap<>();
+        map.put("One", 1);
+        map.put("Two", 2);
+        map.put("Three", 3);
+
+        String key = "Two";
+        int value = map.getOrDefault(key, -1);
+        System.out.println("Value of key '" + key + "': " + value);
+    }
 }
