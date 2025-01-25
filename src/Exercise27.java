d7b4501c1979c67c2a1b0578f0ca2fdf632bf05b
Mirgisa <mirgisakitata@gmail.com>
2025-01-25 13:31:27 +0300

    Java program to remove all mappings from a map.

--- src/Exercise27.java
+++ src/Exercise27.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
 
 public class Exercise27 {
+    public static void main(String[] args) {
+        //27.	Write a Java program to remove all mappings from a map
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("one", 1);
+        map.put("two", 2);
+        map.put("three", 3);
+        System.out.println("Before removing map "+map);
+        map.clear();
+        System.out.println("After Removing "+map);
+    }
 }
