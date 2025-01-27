ad39830cd3869cd11a25aa689efb68419549b155
Mirgisa <mirgisakitata@gmail.com>
2025-01-28 01:16:58 +0300

    Java program to get the current time in New York.

--- src/Exercise36.java
+++ src/Exercise36.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Exercise36 {
+    public static void main(String[] args) {
+        //36.Write a Java program to get the current time in New York
+        ZonedDateTime newYorkTime = ZonedDateTime.now(ZoneId.of("America/New_York"));
+
+        // Format the time
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
+        String formattedTime = newYorkTime.format(formatter);
+
+        // Print the time
+        System.out.println("The current time in New York is: " + formattedTime);
+
+    }
 }
