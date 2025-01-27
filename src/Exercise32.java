e0f6b49791241748408e4c70847f5282ca09b52f
Mirgisa <mirgisakitata@gmail.com>
2025-01-28 01:14:05 +0300

    Java program to get a reverse order view of the keys contained in a given map.

--- src/Exercise32.java
+++ src/Exercise32.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class Exercise32 {
+    public static void main(String[] args) {
+        //32.	Write a Java program to get a reverse order view of the keys contained in a given map
+        Map<String, Integer> map = new HashMap<>();
+        map.put("A", 1);
+        map.put("B", 2);
+        map.put("C", 3);
+
+        List<String> keys = new ArrayList<>(map.keySet());
+        Collections.reverse(keys);
+
+        for (String key : keys) {
+            System.out.println(key);
+        }
+    }
 }
