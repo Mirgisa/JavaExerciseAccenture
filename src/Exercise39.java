6c092850f3dfed37f29b41f09440a37782a148b0
Mirgisa <mirgisakitata@gmail.com>
2025-01-28 01:19:07 +0300

    Java program to get the number of months left in the year.

--- src/Exercise39.java
+++ src/Exercise39.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Exercise39 {
+    public static void main(String[] args) {
+        //39.	Write a Java program to get the number of months left in the year
+        int year = 2025;
+        int currentMonth = 1; // Replace with current month
+
+        int monthsLeftInYear = 12 - currentMonth;
+        System.out.println("Months left in the year: " + monthsLeftInYear);
+    }
 }
